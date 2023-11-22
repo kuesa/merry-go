65076630ee663de738a990facefebb7309a98126
Valen Potloff <38848836+thevaderdude@users.noreply.github.com>
2023-11-22 01:14:47 -0800

    create

--- commands/utility/ch.js
+++ commands/utility/ch.js
@@ -14,10 +14,18 @@ module.exports = {
         let badRegex = /\s\[ch\. \d+\]/g;
         let chapter = interaction.options.getInteger('chapter');
         let newNick;
+        
+        //check bounds
+        if (!(-1 < chapter < 2000)) {
+            await interaction.reply({ content: `Aye Matey! Enter chapters between 0 and 2000!`, ephemeral: true });
 
-        currentNick = currentNick.replace(badRegex, '');
-        newNick = `${currentNick} [ch. ${chapter}]`;
-        await interaction.member.setNickname(newNick);
-        await interaction.reply({ content: `Set you to Chapter ${chapter}!`, ephemeral: true });
+        } else {
+            currentNick = currentNick.replace(badRegex, '');
+            newNick = `${currentNick} [ch. ${chapter}]`;
+            await interaction.member.setNickname(newNick);
+            await interaction.reply({ content: `Set you to Chapter ${chapter}!`, ephemeral: true });
+        }
+
+        
     }
 };
